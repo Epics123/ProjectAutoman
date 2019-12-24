3be12ea637299c5c624a9b5cb03b163e26ad071a
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-12-24 17:23:14 -0500

    Adding ground check to player movement

--- ProjectAutoman/Assets/Scripts/PlayerMovement.cs
+++ ProjectAutoman/Assets/Scripts/PlayerMovement.cs
@@ -18,16 +18,27 @@ public class PlayerMovement : MonoBehaviour
     float fall = 2.5f;
     float gsp = 0.0f;
 
+    bool grounded = false;
+
+    public float xsp, ysp;
+
+    RaycastHit2D aSensor;
+    RaycastHit2D bSensor;
+    RaycastHit2D cSensor;
+    RaycastHit2D dSensor;
+    RaycastHit2D eSensor;
+    RaycastHit2D fSensor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(grounded);
     }
 
     void FixedUpdate()
@@ -37,14 +48,26 @@ public class PlayerMovement : MonoBehaviour
 
     void CheckCollisions()
     {
-        RaycastHit2D aSensor = Physics2D.Raycast(leftSensorStart.position, Vector2.down, vertSensorLength);
-        RaycastHit2D bSensor = Physics2D.Raycast(rightSensorStart.position, Vector2.down, vertSensorLength);
-        RaycastHit2D cSensor = Physics2D.Raycast(leftSensorStart.position, Vector2.up, vertSensorLength);
-        RaycastHit2D dSensor = Physics2D.Raycast(rightSensorStart.position, Vector2.up, vertSensorLength);
-        RaycastHit2D eSensor = Physics2D.Raycast(midSensorStart.position, Vector2.left, horizSensorLength);
-        RaycastHit2D fSensor = Physics2D.Raycast(midSensorStart.position, Vector2.right, horizSensorLength);
+        aSensor = Physics2D.Raycast(leftSensorStart.position, Vector2.down, vertSensorLength);
+        bSensor = Physics2D.Raycast(rightSensorStart.position, Vector2.down, vertSensorLength);
+        cSensor = Physics2D.Raycast(leftSensorStart.position, Vector2.up, vertSensorLength);
+        dSensor = Physics2D.Raycast(rightSensorStart.position, Vector2.up, vertSensorLength);
+        eSensor = Physics2D.Raycast(midSensorStart.position, Vector2.left, horizSensorLength);
+        fSensor = Physics2D.Raycast(midSensorStart.position, Vector2.right, horizSensorLength);
+
+        CheckGround(aSensor, bSensor);
 
         DrawSensors();
+    }
+
+    bool CheckGround(RaycastHit2D sensorA, RaycastHit2D sensorB)
+    {
+        if ((sensorA.collider != null) || (sensorB.collider != null))
+            grounded = true;
+        else
+            grounded = false;
+
+        return grounded;
     }
 
     void DrawSensors()
@@ -64,4 +87,6 @@ public class PlayerMovement : MonoBehaviour
     {
 
     }
+
+
 }
